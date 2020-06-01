d2c8064b01f99dd574c3dc337b8d483b492dd29a
Andrei <a.demyanof@yandex.ru>
2020-06-01 21:29:53 +0300

    Добавьте файлы проекта.

---
+++ Lesson_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 234;
+            int c = 345;
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                if (b > c)
+                {
+                    Console.WriteLine(b);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
